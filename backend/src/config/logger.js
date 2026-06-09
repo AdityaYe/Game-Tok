ff1d21cce283a308ecad23f7840a5d196a8129c2
Aditya Yewale <adityayewale2003@gmail.com>
2026-06-09 14:34:43 +0530

    Deployment reeady

--- backend/src/config/logger.js
+++ backend/src/config/logger.js
@@ -1,19 +1,18 @@
 const pino = require("pino");
 
-const logger = pino({
-  transport: {
-    target: "pino-pretty",
-
-    options: {
-      colorize: true,
-    },
-  },
-
-  level:
-    process.env.NODE_ENV ===
-    "production"
-      ? "info"
-      : "debug",
-});
+const logger =
+  process.env.NODE_ENV === "production"
+    ? pino({
+        level: "info",
+      })
+    : pino({
+        transport: {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+          },
+        },
+        level: "debug",
+      });
 
 module.exports = logger;
